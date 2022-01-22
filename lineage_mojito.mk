9eb4eefa31e955c64385e0cd00ee90374e82dfb5
jhenrique09 <jhsv09@gmail.com>
2022-01-22 20:03:25 +0600

    mojito: Enable quick tap support
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I86fe3b3ad892571f8ccc797b108dffc17019a6c9
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- lineage_mojito.mk
+++ lineage_mojito.mk
@@ -16,6 +16,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Boot Animation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_NAME := lineage_mojito
 PRODUCT_DEVICE := mojito
