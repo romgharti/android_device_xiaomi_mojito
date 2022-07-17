3fb2ee8c9a57df8d2505a80601e525852399f69f
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2022-07-17 22:14:06 +0700

    mojito: Define some flags for RiceDroid
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- lineage_mojito.mk
+++ lineage_mojito.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common RiceDroid stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Boot Animation Resolution
@@ -20,6 +20,21 @@ TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_SUPPORTS_QUICK_TAP := true
 
 TARGET_USES_AOSP_RECOVERY := true
+
+# Chipset flag
+RICE_CHIPSET := "SM6150AC"
+
+# Maintainer flag
+RICE_MAINTAINER := "fiqri1910"
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := true
+
+# Spoof build description/fingerprint as pixel device
+TARGET_USE_PIXEL_FINGERPRINT := false
+
+# GMS build flags
+WITH_GMS := true
 
 PRODUCT_NAME := lineage_mojito
 PRODUCT_DEVICE := mojito
