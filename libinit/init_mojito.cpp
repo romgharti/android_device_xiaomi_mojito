805c49f64bdb0edd7b91202ac6ff2664909a5b25
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2022-10-06 06:19:15 +0700

    mojito: init: Commonize override dalvikvm heapsize and heapminfree props
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- libinit/init_mojito.cpp
+++ libinit/init_mojito.cpp
@@ -41,19 +41,18 @@ void load_dalvikvm_props() {
         // from - phone-xhdpi-6144-dalvik-heap.mk
         property_override("dalvik.vm.heapstartsize", "16m");
         property_override("dalvik.vm.heapgrowthlimit", "256m");
-        property_override("dalvik.vm.heapsize", "512m");
         property_override("dalvik.vm.heaptargetutilization", "0.5");
-        property_override("dalvik.vm.heapminfree", "8m");
         property_override("dalvik.vm.heapmaxfree", "32m");
     } else if (sys.totalram > 3072ull * 1024 * 1024) {
         // from - phone-xhdpi-4096-dalvik-heap.mk
         property_override("dalvik.vm.heapstartsize", "8m");
         property_override("dalvik.vm.heapgrowthlimit", "192m");
-        property_override("dalvik.vm.heapsize", "512m");
         property_override("dalvik.vm.heaptargetutilization", "0.6");
-        property_override("dalvik.vm.heapminfree", "8m");
         property_override("dalvik.vm.heapmaxfree", "16m");
     }
+
+    property_override("dalvik.vm.heapsize", "512m");
+    property_override("dalvik.vm.heapminfree", "8m");
 }
 
 void load_vendor_props() {
